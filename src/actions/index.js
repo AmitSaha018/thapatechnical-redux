fa9024e0505fcf6d514dbd017102506346842b64
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-03-18 21:55:11 +0530

    Creating action Creators.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const increment = (num) => {
+    return {
+        type: 'INCREASE',
+        payload: num
+    }
+};
+export const decrement = (num) => {
+    return {
+        type: 'DECREASE'
+    }
+};
