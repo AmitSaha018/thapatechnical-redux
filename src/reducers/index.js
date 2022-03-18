e2053141a76f0f7920f45be317b476ef4a4766b9
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-03-18 22:03:58 +0530

    Combining multiple reducers if available.

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import changeTheNumber from './updown';
+
+const rootReducer = combineReducers({
+    //   myNumber:changeTheNumber
+    changeTheNumber
+});
+
+export default rootReducer;
+
